a53f26027d6358ec99559eeb5b631db59f50f900
Joe Vander Veen <joevdiscgolf@gmail.com>
2026-01-29 16:31:54 -0500

    Remove testing code in has onboarded logic

--- lib/repositories/firebase_auth_repository.dart
+++ lib/repositories/firebase_auth_repository.dart
@@ -278,8 +278,7 @@ class FirebaseAuthRepository implements AuthRepository {
 
   @override
   bool userHasOnboarded() {
-    return false;
-    // return getCurrentUser()?.displayName?.contains('has_onboarded') == true;
+    return getCurrentUser()?.displayName?.contains('has_onboarded') == true;
   }
 
   @override
